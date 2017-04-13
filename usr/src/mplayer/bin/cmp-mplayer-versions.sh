ce19057c79a2c413cb0580097f3566f4c77a76a0
BevapDin <Tho_Ki@gmx.de>
2017-04-13 23:15:05 +0200

    dummy: move first

--- usr/src/mplayer/bin/cmp-mplayer-versions.sh
+++ usr/src/mplayer/bin/cmp-mplayer-versions.sh
@@ -1,12 +1,26 @@
 #! /bin/bash
+#
+# Compares two mplayer binaries based on the codes they support.
 
-BIN1="$(which mplayer)"
-BIN2="./mplayer"
-LOG="/tmp/mplayer-cmp-$$-"
-out_cmd="less -S"
-store_results=""
+# The two mplayer binaries, first is the currently installed one (the bash sees it),
+# second is the one in the current directory.
+BINARY1="$(which mplayer)"
+BINARY2="./mplayer"
+# Command to display the comparison
+VIEW_COMMAND="less -S"
 
-resumefnc() {
+usage() {
+	cat <<- HERE
+	Usage: $0 {<options>}
+	Options:
+	-1 <path> ... First binary to compare.
+	-2 <path> ... Second binary to compare.
+	--batch ... Only show differing lines, don't use less for display, just print the text.
+HERE
+}
+
+# Only show lines that contain a difference (as reported by diff).
+grep_differing() {
 	grep -E '[<>|]'
 }
 
@@ -14,12 +28,20 @@ while [ "$#" != 0 ] ; do
 	opt="$1"
 	shift 1
 	case "$opt" in
-		--store)
-			store_results="$1"
+		--batch)
+			VIEW_COMMAND="grep_differing"
+			;;
+		-1)
+			BINARY1="$1"
 			shift 1
 			;;
-		--batch)
-			out_cmd="resumefnc"
+		-2)
+			BINARY2="$1"
+			shift 1
+			;;
+		--help|-h|-\?)
+			usage
+			exit 1
 			;;
 		*)
 			echo "Unknown option: $opt"
@@ -28,26 +50,29 @@ while [ "$#" != 0 ] ; do
 	esac
 done
 
-makelist() {
+# Out the codes available in binary $1, using "mplayer -$cmd help" as command line,
+# cmd is the second parameter.
+make_list() {
 	local binary="$1"
 	local cmd="$2"
 	date
-	echo "$binary $cmd"
+	echo "$binary -$cmd help"
 	$binary -$cmd help 2>&1 | \
 		tail -n +3 | \
 		sort
 }
 
-for d in ao ac vo vc ; do
-	l1="$LOG-$d.1"
-	l2="$LOG-$d.2"
-	makelist "$BIN1" "$d" > "$l1"
-	makelist "$BIN2" "$d" > "$l2"
-	diff -W 100 -w -y "$l1" "$l2" 2>&1 | $out_cmd
-	if [ -n "$store_results" ] ; then
-		cp "$l2" "$store_results"
-	fi
-	rm "$l1" "$l2"
-done
+# Create the comparison using diff, for the codes $cmd (first parameter).
+do_diff() {
+	local cmd="$1"
+	diff -W 100 -w -y <(make_list "$BINARY1" "$cmd") <(make_list "$BINARY2" "$cmd" 2>&1)
+}
+
+# For each code:
+# audio output (not actually a code), audio code, video output (not actually a code), video code
+# Pipe everything to the view command.
+for cmd in ao ac vo vc ; do
+	do_diff "$cmd"
+done | $VIEW_COMMAND
 
 exit 0
--- usr/src/mplayer/bin/daily-svn-update.sh
+++ usr/src/mplayer/bin/daily-svn-update.sh
@@ -1,24 +1,43 @@
 #! /bin/bash
 
+# Update the mplayer package from current svn, completely automated.
+# This is intended to be run from a cron job. Running it from the console is also possible.
+
 export PATH="$HOME/bin:$PATH"
 
-working_dir="/usr/src/mplayer/mplayer@svn"
+# Where the svn checkout is.
+SVN_CHECKOUT_DIR="/usr/src/mplayer/mplayer@svn"
 
-cd "$working_dir" || exit $?
+if ! -e [ "$SVN_CHECKOUT_DIR" ] ; then
+	svn checkout "svn://svn.mplayerhq.hu/mplayer/trunk" "$SVN_CHECKOUT_DIR" || exit $?
+fi
 
+cd "$SVN_CHECKOUT_DIR" || exit $?
+
+# Can be set to prevent this script from running, useful when this script is called
+# from a cron job. It simulates success, so cron does not complain.
+if [ -e ".no-daily-svn-update" ] ; then
+	exit 0
+fi
+
+# Update from svn server
 svn up || exit $?
 
 make clean
 if ! cfgc --no-install --quiet ; then
-	ret="$?"
+	# If make (or configure) failed, print the log files and the environment
+	echo "Output of make:"
 	cat make.{1,2}.log
-	exit "$ret"
+	echo "Environment:"
+	env
+	exit 1
 fi
 
+# Just for the fun: compare the new and the current binary.
 cmp-mplayer-versions.sh --batch
 
+# Install binary, but make a backup, using explicit /bin/cp to avoid the wrappers for the package user.
 for d in mplayer mencoder ; do
 	/bin/cp --remove-destination --backup=numbered "/usr/bin/$d" "$HOME/$d.bak"
 	/bin/cp --remove-destination "$d" "/usr/bin/$d"
 done
-
